808758e31d5f02df85a775265fe837b6ec3e461c
whoparthgarg <parthgarg2013@gmail.com>
2021-02-25 14:21:14 +0530

    Recursion

---
+++ 6. Recursion/4_Print_1_to_N.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)//time complexity O(n)
+{
+    if(n==0)
+    {
+        return ; 
+    }
+    
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    print(n);
+    
+    return 0;
+}
