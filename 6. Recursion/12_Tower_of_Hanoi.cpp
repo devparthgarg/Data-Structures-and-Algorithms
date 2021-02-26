90d42c745597d6b5467f0ed475f68238cc9ed90d
whoparthgarg <parthgarg2013@gmail.com>
2021-02-26 12:17:18 +0530

    Recursion

---
+++ 6. Recursion/12_Tower_of_Hanoi.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void towerOfHanoi(int n,char A,char B,char C)
+{
+    if(n==1)
+    {
+        cout<<"Move 1 from "<<A<<" to "<<C<<endl;  //taking C as auxiliary
+        return ;
+    }
+
+    towerOfHanoi(n-1,A,C,B);
+    cout<<"Move "<<n<<" from "<<A<<" to "<<C<<endl;
+    towerOfHanoi(n-1,B,A,C);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    towerOfHanoi(n,'A','B','C');
+    return 0;
+}
