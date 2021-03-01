d30c542d9d0f44202f295a0432ab9b938e9166cf
whoparthgarg <parthgarg2013@gmail.com>
2021-03-01 13:01:37 +0530

    SecondLargest

---
+++ 8. Arrays/5_Second_Largest_Element.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largest(int a[],int n)//time comp. O(n)
+{
+    int max=INT_MIN;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+
+    return max;
+}
+
+int secondLargest(int arr[],int n)
+{
+    int max=largest(arr,n);
+    int res=0;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        if(res<arr[i] and arr[i]!=max)
+        {
+            res=arr[i];
+        }
+    }
+    if(i==n)
+    {
+        return -1;
+    }
+
+    return res;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<secondLargest(arr,n);
+    return 0;
+}
