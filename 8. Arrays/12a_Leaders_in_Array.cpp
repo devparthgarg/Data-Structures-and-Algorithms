c979bc28ba863b2b2e7546f4b97c746f3eccfe82
whoparthgarg <parthgarg2013@gmail.com>
2021-03-04 09:36:43 +0530

    Leaders Naive Solution

---
+++ 8. Arrays/12a_Leaders_in_Array.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//leader - nothing greater in right of it (equals are not allowed)
+void leaders(int *a,int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        bool flag=true;
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[j]>=a[i])
+            {
+                flag=false;
+            }
+        }
+        if(flag==true)
+        {
+            cout<<a[i]<<" ";
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    leaders(a,n);
+    return 0;
+}
