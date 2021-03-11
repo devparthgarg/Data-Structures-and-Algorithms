adca8201082aa2c9fff331790cca454647c62081
whoparthgarg <parthgarg2013@gmail.com>
2021-03-11 15:22:15 +0530

    double pointer

---
+++ 9. Matrix/1c_Double_Pointer.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m=3,n=2;
+    int **arr;//double pointer : stores address of pointer
+    
+    arr=new int*[m];
+    for(int i=0;i<m;i++)
+    {
+        arr[i]=new int[n];
+    }
+
+    int val=1;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            arr[i][j]=val;
+            cout<<arr[i][j]<<" ";
+            val++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
