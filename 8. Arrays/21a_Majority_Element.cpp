da6319132ea435cf87da0fd1f6d3ffcdea0e02ea
whoparthgarg <parthgarg2013@gmail.com>
2021-03-18 22:24:10 +0530

    naive solution

---
+++ 8. Arrays/21a_Majority_Element.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int majorityElement(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        int count=1;
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+            }
+        }
+        if(count>(n/2))
+        {
+            return i;
+        }
+    }
+    
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<majorityElement(arr,n);    
+    return 0;
+}
