0a872e9746e3489baffac537a121515c17390009
whoparthgarg <parthgarg2013@gmail.com>
2021-04-16 10:19:59 +0530

    Minimum Coins Indian Currency

--- 10. Greedy Algorithms/1_Minimum_Coins.cpp
+++ 10. Greedy Algorithms/1_Minimum_Coins.cpp
@@ -6,41 +6,36 @@ bool compare(int a,int b)
     return a>b;
 }
 
-int minCoins(int *coins,int n,int amount)
+int minimumCoins(int *coins,int n,int amount)
 {
-    //1. sort in decreasing order
+    //sort coins in decreasing order
     sort(coins,coins+n,compare);
 
-    //2. intitalize result
     int res=0;
 
-    //3. compare each coin and update remaining amount
     for(int i=0;i<n;i++)
     {
         if(coins[i]<=amount)
         {
-            int c=amount/coins[i];
-            res=res+c;
-            amount=amount-c*coins[i];
+            int count=amount/coins[i];
+            res=res+count;
+            amount=amount-count*coins[i];
         }
+
         if(amount==0)
         {
             break;
         }
     }
-
     return res;
 }
 
 int main()
 {
     int coins[]={1,2,5,10,20,50,100,200,500,2000};
-
     int n=sizeof(coins)/sizeof(int);
+    int amount=2888;
 
-    int amount;
-    cin>>amount;
-
-    cout<<minCoins(coins,n,amount);    
+    cout<<minimumCoins(coins,n,amount);
     return 0;
 }
