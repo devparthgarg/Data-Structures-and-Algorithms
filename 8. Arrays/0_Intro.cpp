661d4983daf511022038a6a018df1c197eb45e09
whoparthgarg <parthgarg2013@gmail.com>
2021-04-21 11:09:16 +0530

    array advantage

---
+++ 8. Arrays/0_Intro.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    /*
+    
+    Array Advantages :
+
+    1.Continous memory
+    2.Random access
+    3.Cache friendliness
+
+    Types of arrays :
+
+    1.fixed size arrays , eg: int arr[100]
+    2.dynamic size arrays , eg: vector<int> v
+    
+    */
+}
