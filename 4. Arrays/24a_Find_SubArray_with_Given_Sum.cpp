ec9f20bf9f4df77e735515cb4f31528dc22deec4
whoparthgarg <parthgarg2013@gmail.com>
2021-04-24 11:44:23 +0530

    naive solution

---
+++ 4. Arrays/24a_Find_SubArray_with_Given_Sum.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+given an array(unsorted and non-negative integers) 
+and a integer k find the maximum
+sum of k consecutive elements
+*/
+bool subArray(int a[],int n,int sum)//time comp. O(n^2)
+{
+    for(int i=0;i<n;i++)
+    {
+        int currSum=0;
+        for(int j=i;j<n;j++)
+        {
+            currSum=currSum+a[j];
+            if(currSum==sum)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+
+    int sum;
+    cin>>sum;
+
+    cout<<subArray(a,n,sum);
+    return 0;
+}
