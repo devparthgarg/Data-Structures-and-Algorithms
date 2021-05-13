72b5990b8ac949d52f9f0dcc01eb89cb1afcd761
whoparthgarg <parthgarg2013@gmail.com>
2021-05-13 15:32:09 +0530

    more methods added

--- 1. Mathematics/1_Find_Number_of_Digits.cpp
+++ 1. Mathematics/1_Find_Number_of_Digits.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int countDigits(int n)
+/*
+3 methods :
+
+a.iterative
+b.recursive
+c.logarithmic
+*/
+
+int countDigits(int n)//iterative solution
 {
     int cnt=0;
     while(n!=0)
@@ -13,11 +21,28 @@ int countDigits(int n)
     return cnt;
 }
 
+int countDigits2(int n)//recursive solution
+{
+    if(n==0)
+    {
+        return 0;
+    }
+
+    return 1+countDigits2(n/10);
+}
+
+int countDigits3(int n)//logarithmic solution
+{
+    return floor(log10(n)+1);
+}
+
 int main()
 {
     int n;
     cin>>n;
 
-    cout<<countDigits(n);
+    cout<<countDigits(n)<<endl;
+    cout<<countDigits2(n)<<endl;
+    cout<<countDigits3(n)<<endl;
     return 0;
 }
