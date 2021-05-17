ab003caa285e3b285d4b28bb8fe2b69ce8de93e1
whoparthgarg <parthgarg2013@gmail.com>
2021-05-17 18:39:57 +0530

    unordered map

---
+++ 8. Hashing/2_Unordered_Map.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	/*
+	properties :
+
+	1.used to store key-value pair
+	2.based on hashing
+	3.no order of keys
+	4.on average all operations O(1)
+
+	syntax :
+
+	unordered_map<key,value> name;
+	*/
+
+	unordered_map<string,int> m;
+
+	m["parth"]=13;
+	m["aditya"]=6;
+	m.insert({"akhil",21});
+
+	//printing : ordered in which pairs is stored may be different
+	for(auto x:m)
+	{
+		cout<<x.first<<" "<<x.second<<endl;
+	}
+	cout<<"---"<<endl;
+
+	for(auto it=m.begin();it!=m.end();it++)
+	{
+		cout<<it->first<<" "<<it->second<<endl;
+	}
+	cout<<"---"<<endl;
+
+	//find function
+	if(m.find("aditya")==m.end())
+	{
+		cout<<"not found"<<endl;
+	}
+	else
+	{
+		cout<<"found"<<endl;
+	}
+	cout<<"---"<<endl;
+
+	//count function : return type is size_t
+	if(m.count("akhil")>0)
+	{
+		cout<<"found"<<endl;
+	}
+	else
+	{
+		cout<<"not found"<<endl;
+	}
+	cout<<"---"<<endl;
+
+	//size function
+	cout<<m.size()<<endl;
+	cout<<"---"<<endl;
+
+	//erase function
+	m.erase("aditya");
+	m.erase(m.begin());
+
+	cout<<m.size()<<endl;
+	cout<<"---"<<endl;
+
+	for(auto x:m)
+	{
+		cout<<x.first<<" "<<x.second<<endl;
+	}
+	return 0;
+}
