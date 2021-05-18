1e75d4fb2e45df7cbb6554354d2e608154ab5be6
whoparthgarg <parthgarg2013@gmail.com>
2021-05-18 13:48:19 +0530

    pair sum

---
+++ 8. Hashing/7b_Pair_with_given_Sum.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool pairSum(int a[],int n,int sum)//time comp. O(n)
+{
+	unordered_set<int> m;
+
+	for(int i=0;i<n;i++)
+	{
+		if(m.find(sum-a[i])!=m.end())
+		{
+			return true;
+		}
+		else
+		{
+			m.insert(a[i]);
+		}
+	}
+	
+	return false;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+
+	int sum;
+	cin>>sum;
+
+	cout<<pairSum(arr,n,sum);
+	return 0;
+}
