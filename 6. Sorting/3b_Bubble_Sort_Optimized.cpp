d96a7e228912dc0ec4984a0a550ca04994f72153
whoparthgarg <parthgarg2013@gmail.com>
2021-05-24 17:42:01 +0530

    stable algo O(n^2)

---
+++ 6. Sorting/3b_Bubble_Sort_Optimized.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int a[],int n)//time comp. O(n^2) ; space comp. O(1) ; stable algorithm
+{
+	for(int i=0;i<n-1;i++)//n-1 passes
+	{
+		bool flag=false;
+		for(int j=0;j<n-i-1;j++)//for bubbling(largest element at last -> second largest at it correct positon...)
+		{
+			if(a[j]>a[j+1])
+			{
+				swap(a[j],a[j+1]);
+				flag=true;
+			}
+		}
+		if(flag==false)
+		{
+			break;
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+
+	bubbleSort(a,n);
+
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+	return 0;
+}
