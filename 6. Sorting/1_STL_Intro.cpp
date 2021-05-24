06f83e5772209b452b4c9525d77f1f4b353b8d61
whoparthgarg <parthgarg2013@gmail.com>
2021-05-24 16:10:38 +0530

    into sort

---
+++ 6. Sorting/1_STL_Intro.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	/*
+	Inbuilt sort function features :
+
+	1. worst and average time comp. O(nlogn)
+	2. uses Intro-Sort (hybrid of heapsort , quicksort and insertion sort)
+	*/
+
+	return 0;
+}
