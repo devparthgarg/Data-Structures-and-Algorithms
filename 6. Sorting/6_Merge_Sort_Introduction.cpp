fd78f83f551ba18bec74f67a1e0b6dabd7941d3b
whoparthgarg <parthgarg2013@gmail.com>
2021-05-25 23:29:13 +0530

    merge sort features

---
+++ 6. Sorting/6_Merge_Sort_Introduction.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	/*
+	
+	Features :
+
+	1.divide and conquer algorithm
+
+	2.3steps :
+		a.divide
+		b.sort
+		c.merge
+	
+	3.stable algorithm
+	
+	4.Time comp. O(n*logn)
+	
+	5.Space comp. O(n)
+	
+	6.For arrays : Quicksort is perform better
+	than merge sort
+	
+	*/
+
+	return 0;
+}
