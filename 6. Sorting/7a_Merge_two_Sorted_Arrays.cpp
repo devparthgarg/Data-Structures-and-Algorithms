acc126eb296ffe671a94c648b3d264182d0e59b2
whoparthgarg <parthgarg2013@gmail.com>
2021-05-25 23:30:18 +0530

    naive and efficient approaches

---
+++ 6. Sorting/7a_Merge_two_Sorted_Arrays.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int a[],int b[],int m,int n)//time comp. O((m+n)*log(m+n)) ; space comp. O(m+n)
+{
+	int c[m+n];
+
+	for(int i=0;i<m;i++)
+	{
+		c[i]=a[i];
+	}
+
+	for(int i=0;i<n;i++)
+	{
+		c[m+i]=b[i];
+	}
+
+	sort(c,c+m+n);
+
+	for(int i=0;i<(m+n);i++)
+	{
+		cout<<c[i]<<" ";
+	}
+}
+
+int main()
+{
+	int a1[]={1,3,5,7,9};
+	int a2[]={2,4,6,8};
+
+	int n1=sizeof(a1)/sizeof(int);
+	int n2=sizeof(a2)/sizeof(int);
+
+	merge(a1,a2,n1,n2);
+	return 0;
+}
---
+++ 6. Sorting/7b_Merge_two_Sorted_Arrays.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int a[],int b[],int m,int n)//time comp. O(m+n) ; space comp. O(1)
+{
+	int i=0;
+	int j=0;
+
+	while(i<m and j<n)
+	{
+		if(a[i]<=b[j])
+		{
+			cout<<a[i]<<" ";
+			i++;
+		}
+		else
+		{
+			cout<<b[j]<<" ";
+			j++;
+		}
+	}
+
+	//print remaining of array a and b
+	while(i<m)
+	{
+		cout<<a[i]<<" ";
+		i++;
+	}
+
+	while(i<n)
+	{
+		cout<<a[j]<<" ";
+		j++;
+	}
+}
+
+int main()
+{
+	int a1[]={1,3,5,7,9};
+	int a2[]={2,4,6,8};
+
+	int n1=sizeof(a1)/sizeof(int);
+	int n2=sizeof(a2)/sizeof(int);
+
+	merge(a1,a2,n1,n2);
+	return 0;
+}
