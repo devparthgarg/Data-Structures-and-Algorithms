4e5cca472b8812434c352bd9920630a948a909d4
whoparthgarg <parthgarg2013@gmail.com>
2021-05-27 18:01:27 +0530

    eff. O(m+n)

---
+++ 6. Sorting/10a_Intersection_of_two_Sorted_Arrays.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void intersection(int a[],int b[],int m,int n)//time comp. O(m*n) ; space comp. O(1)
+{
+	for(int i=0;i<m;i++)
+	{
+		if(i>0 and a[i]==a[i-1])
+		{
+			continue;
+		}
+
+		for(int j=0;j<n;j++)
+		{
+			if(a[i]==b[j])
+			{
+				cout<<a[i]<<" ";
+				break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[]={3,5,10,10,10,15,15,20};
+	int b[]={5,10,10,15,30};
+
+	int m=sizeof(a)/sizeof(int);
+	int n=sizeof(b)/sizeof(int);
+
+	intersection(a,b,m,n);
+	return 0;
+}
---
+++ 6. Sorting/10b_Intersection_of_two_Sorted_Arrays.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void intersection(int a[],int b[],int m,int n)//time comp. O(m+n) ; space comp. O(1)
+{
+	int i=0,j=0;
+
+	while(i<m and j<n)
+	{
+		if(i>0 and a[i]==a[i-1])
+		{
+			i++;
+			continue;
+		}
+
+		if(a[i]<b[j])
+		{
+			i++;
+		}
+		else if(a[i]>b[j])
+		{
+			j++;
+		}
+		else
+		{
+			cout<<a[i]<<" ";
+			i++;
+			j++;
+		}
+	}
+}
+
+int main()
+{
+	int a[]={3,5,10,10,10,15,15,20};
+	int b[]={5,10,10,15,30};
+
+	int m=sizeof(a)/sizeof(int);
+	int n=sizeof(b)/sizeof(int);
+
+	intersection(a,b,m,n);
+	return 0;
+}
