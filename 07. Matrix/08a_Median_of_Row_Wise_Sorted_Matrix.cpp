52e1bbb16c0884a058c3392c845c3f64f5e22d93
unknown <Parth Garg>
2021-07-11 00:34:10 +0530

    matrix

---
+++ 07. Matrix/08a_Median_of_Row_Wise_Sorted_Matrix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*Assumptions :
+1.odd sized arrays
+2.all distinct elements*/
+int median(vector<vector<int>>& v)//time comp. O(n^2)
+{
+	vector<int> arr;
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		for (int j = 0; j < v[i].size(); j++)
+		{
+			arr.push_back(v[i][j]);
+		}
+	}
+
+	sort(arr.begin(), arr.end());
+
+	return arr[arr.size() / 2];
+}
+
+int main()
+{
+	vector<vector<int>> v = {
+		{2, 4, 6, 8, 10},
+		{1, 3, 5, 7, 9},
+		{100, 200, 400, 500, 800}
+	};
+
+	cout << median(v);
+	return 0;
+}
