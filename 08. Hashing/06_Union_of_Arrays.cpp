89ca9d441819e7cb4878242718e548ddcfb06d46
whoparthgarg <parthgarg2013@gmail.com>
2021-07-11 16:08:44 +0530

    hashing

--- 08. Hashing/06_Union_of_Arrays.cpp
+++ 08. Hashing/06_Union_of_Arrays.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int unionArray(int a[],int b[],int m,int n)//time comp. O(n)
+int unionArray(int a[], int b[], int m, int n) //time comp. O(n)
 {
-	unordered_set<int> s(a,a+m);
+	unordered_set<int> s(a, a + m);
 
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
 		s.insert(b[i]);
 	}
@@ -15,22 +15,22 @@ int unionArray(int a[],int b[],int m,int n)//time comp. O(n)
 
 int main()
 {
-	int m,n;
-	cin>>m>>n;
+	int m, n;
+	cin >> m >> n;
 
 	int a[m];
 	int b[n];
 
-	for(int i=0;i<m;i++)
+	for (int i = 0; i < m; i++)
 	{
-		cin>>a[i];
+		cin >> a[i];
 	}
 
-	for(int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
-		cin>>b[i];
+		cin >> b[i];
 	}
 
-	cout<<unionArray(a,b,m,n);
+	cout << unionArray(a, b, m, n);
 	return 0;
 }
