b805d71c8ed6535233ded4eb9c5ee95a6459f35f
whoparthgarg <parthgarg2013@gmail.com>
2021-07-21 12:48:47 +0530

    deque

---
+++ 13. Deque/02_Deque_STL.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//printing using "for each" loop
+void print(deque<int> dq)
+{
+	for (auto x : dq)
+	{
+		cout << x << " ";
+	}
+	cout << endl;
+}
+
+int main()
+{
+	//declaration
+	deque<int> dq = {1, 2, 3};
+
+	//push_front()
+	dq.push_front(4);
+	print(dq);
+
+	//push_back()
+	dq.push_back(5);
+	print(dq);
+
+	//pop_front()
+	dq.pop_front();
+	print(dq);
+
+	//pop_back()
+	dq.pop_back();
+	print(dq);
+
+	//front()
+	cout << dq.front() << endl;
+
+	//back()
+	cout << dq.back() << endl;
+
+	//size()
+	cout << dq.size() << endl;
+
+	//max_size()
+	cout << dq.max_size() << endl;
+
+	//empty()
+	if (dq.empty())
+	{
+		cout << "deque is empty" << endl;
+	}
+	else
+	{
+		cout << "deque is not empty" << endl;
+	}
+
+	//random access
+	cout << dq[1] << endl;
+
+	return 0;
+}
