917a0fe64b8b16f30f05482926fef30a861a2fd2
whoparthgarg <parthgarg2013@gmail.com>
2021-07-21 12:19:29 +0530

    deque

---
+++ 13. Deque/01_Intro.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	/*
+	Introduction:
+	deque : doubly ended queue
+	meaning that it has two ends : front and rear
+	we can perform insert/delete on both ends.
+
+	Basic Operations:
+	1.push_front()
+	2.push_back()
+	3.pop_front()
+	4.pop_back()
+
+	More Operations:
+	1.front()
+	2.back()
+	3.size()
+	4.max_size()
+	5.empty()
+
+	note: all operations mentioned above are O(1) operations
+
+	Implementation :
+	1.Linked List (doubly)
+	2.Array (circular)
+	*/
+
+	return 0;
+}
