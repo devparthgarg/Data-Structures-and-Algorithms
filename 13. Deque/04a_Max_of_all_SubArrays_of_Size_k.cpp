c9d288d2fdddd642d18833a2abaad3aeab22436b
whoparthgarg <parthgarg2013@gmail.com>
2021-07-21 16:02:05 +0530

    deque

---
+++ 13. Deque/04a_Max_of_all_SubArrays_of_Size_k.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Print all maximum in size of subarray k
+void maxOfSubArrays(vector<int> v, int k)//time comp. O(n^2) ; space comp. O(1)
+{
+	int n = v.size();
+	for (int i = 0; i < n - k + 1; i++)
+	{
+		int max_no = INT_MIN;
+		for (int j = i; j < i + k; j++)
+		{
+			max_no = max(max_no, v[j]);
+		}
+		cout << max_no << " ";
+	}
+}
+
+int main()
+{
+	vector<int> v = {10, 8, 5, 12, 15, 7, 6};
+	int k = 3;
+
+	maxOfSubArrays(v, k);
+	return 0;
+}
