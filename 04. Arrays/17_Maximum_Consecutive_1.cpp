6c5847d719acac4dd2cdf4b5e078fd30ee04252c
whoparthgarg <parthgarg2013@gmail.com>
2021-07-23 09:53:40 +0530

    arrays

--- 04. Arrays/17_Maximum_Consecutive_1.cpp
+++ 04. Arrays/17_Maximum_Consecutive_1.cpp
@@ -1,39 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int maxConsecutive1(int *a,int n)//time comp. O(n)
+int maxConsecutive1(int *a, int n) //time comp. O(n)
 {
-   int count=0;
-   int maxi=0;
-   for(int i=0;i<n;i++)
-   {
-       if(a[i]==0)
-       {
-           count=0;
-           continue;
-       }
+    int res = 0, curr = 0;
 
-       count++;
-       if(count>maxi)
-       {
-           maxi=count;
-       }
-   }
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == 0)
+        {
+            curr = 0;
+        }
+        else
+        {
+            curr++;
+            res = max(res, curr);
+        }
+    }
 
-   return maxi;
+    return res;
 }
 
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
 
     int arr[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
 
-    cout<<maxConsecutive1(arr,n)<<endl;
+    cout << maxConsecutive1(arr, n) << endl;
     return 0;
 }
