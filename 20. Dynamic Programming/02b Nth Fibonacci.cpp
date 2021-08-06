c246aa9af3f0f4f5d2e4480eb0f1b93aa29d2430
whoparthgarg <parthgarg2013@gmail.com>
2021-08-06 09:34:45 +0530

    dp

---
+++ 20. Dynamic Programming/02b Nth Fibonacci.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int memo[100000];
+
+//Memoization
+int fib(int n)//time comp. O(n) ; space comp. O(n)
+{
+	if (memo[n] == -1)
+	{
+		int res;
+		if (n == 0 or n == 1)
+		{
+			res = n;
+		}
+		else
+		{
+			res = fib(n - 1) + fib(n - 2);
+		}
+		memo[n] = res;
+	}
+
+	return memo[n];
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	memset(memo, -1, sizeof(memo)); //fill memo array with -1
+
+	cout << fib(n) << endl;
+	return 0;
+}
--- 20. Dynamic Programming/02c Nth Fibonacci.cpp
+++ 20. Dynamic Programming/02c Nth Fibonacci.cpp
@@ -1,17 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Tabulation
 int fib(int n)//time comp. O(n) ; space comp. O(n)
 {
 	int f[10000];
-	memset(f,-1,sizeof(f));
+	memset(f, -1, sizeof(f));
 
-	f[0]=0;
-	f[1]=1;
+	f[0] = 0;
+	f[1] = 1;
 
-	for(int i=2;i<=n;i++)
+	for (int i = 2; i <= n; i++)
 	{
-		f[i]=f[i-1]+f[i-2];
+		f[i] = f[i - 1] + f[i - 2];
 	}
 
 	return f[n];
@@ -19,10 +20,9 @@ int fib(int n)//time comp. O(n) ; space comp. O(n)
 
 int main()
 {
-	// nth fibonacci number
 	int n;
-	cin>>n;
+	cin >> n;
 
-	cout<<fib(n)<<endl;
+	cout << fib(n) << endl;
 	return 0;
 }
