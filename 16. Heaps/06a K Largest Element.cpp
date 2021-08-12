f1482ddaba6c113ced728299f3fa1bde8ec8b249
whoparthgarg <parthgarg2013@gmail.com>
2021-08-12 17:46:03 +0530

    heaps

---
+++ 16. Heaps/06a K Largest Element.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Naive Method
+void kLargest(int a[], int n, int k)//time comp. O(n*logn) ; space comp. O(1)
+{
+	sort(a, a + n);
+
+	int i = n - 1;
+	while (k--)
+	{
+		cout << a[i] << " ";
+		i--;
+	}
+}
+
+int main()
+{
+	int a[] = {5, 15, 10, 20, 8};
+	int n = sizeof(a) / sizeof(int);
+	int k = 2;
+
+	kLargest(a, n, k);
+	return 0;
+}
