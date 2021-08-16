79d006877d83fc88aa773c5950c9a21fcfdfe514
whoparthgarg <parthgarg2013@gmail.com>
2021-08-16 11:18:00 +0530

    strings

---
+++ 09. Strings/14a Check if Strings are Rotation.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotate(string &s)
+{
+	char ch = s[0];
+	for (int i = 1; i < s.length(); i++)
+	{
+		s[i - 1] = s[i];
+	}
+	s[s.length() - 1] = ch;
+}
+
+bool isRotated(string s1, string s2)//time comp. O(n^2) ; space comp. O(1)
+{
+	if (s1 == s2)
+	{
+		return true;
+	}
+	if (s1.length() != s2.length())
+	{
+		return false;
+	}
+
+	int n = s1.length();
+	for (int i = 0; i < n; i++)
+	{
+		if (s1 == s2)
+		{
+			return true;
+		}
+		else
+		{
+			//rotate s2
+			rotate(s2);
+		}
+	}
+
+	return false;
+}
+
+int main()
+{
+	string s1 = "ABCD";
+	string s2 = "CDAB";
+
+	cout << isRotated(s1, s2);
+	return 0;
+}
---
+++ 09. Strings/14b Check if Strings are Rotation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isRotated(string s1, string s2)//time comp. O(n) ; space comp. O(m+n)
+{
+	if (s1 == s2)
+	{
+		return true;
+	}
+	if (s1.length() != s2.length())
+	{
+		return false;
+	}
+
+	string temp = s1 + s2;
+
+	return temp.find(s2) != string::npos;
+}
+
+int main()
+{
+	string s1 = "ABCD";
+	string s2 = "CDAB";
+
+	cout << isRotated(s1, s2);
+	return 0;
+}
