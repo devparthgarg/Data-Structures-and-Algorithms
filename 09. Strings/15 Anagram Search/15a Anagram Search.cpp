ede809a33b66fd97a2eb1f53a1abca4cef43b054
whoparthgarg <parthgarg2013@gmail.com>
2021-11-16 15:38:13 +0530

    strings

---
+++ 09. Strings/15 Anagram Search/15a Anagram Search.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s1, string s2)//time comp. O(n)
+{
+	if (s1.length() != s2.length())
+	{
+		return false;
+	}
+
+	int count[256] = {0};
+	int n = s1.length();
+
+	for (int i = 0; i < n; i++)
+	{
+		count[s1[i]]++;
+		count[s2[i]]--;
+	}
+
+	for (int i = 0; i < 256; i++)
+	{
+		if (count[i] != 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool isAnagramPresent(string &txt, string &pat)//time comp. O((n-m+1)*m) ; space comp. O(1)
+{
+	int m = txt.length();
+	int n = pat.length();
+
+	for (int i = 0; i <= m - n; i++)
+	{
+		if (isAnagram(txt.substr(i, n), pat))
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+int main()
+{
+	string txt = "parthgarg";
+	string pat = "thar";
+
+	cout << isAnagramPresent(txt, pat) << endl;
+	return 0;
+}
---
+++ 09. Strings/15 Anagram Search/15b Anagram Search.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSame(int a1[], int a2[])//time comp. O(1) ; constant because 256 is fixed
+{
+	for (int i = 0; i < 256; i++)
+	{
+		if (a1[i] != a2[i])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+bool isAnagramPresent(string &txt, string &pat)//time comp. O(m) ; space comp. O(1)
+{
+	int m = txt.length();
+	int n = pat.length();
+
+	int cnt_txt[256] = {0};
+	int cnt_pat[256] = {0};
+
+	for (int i = 0; i < n; i++)
+	{
+		cnt_txt[txt[i]]++;
+		cnt_pat[pat[i]]++;
+	}
+
+	bool flag = isSame(cnt_txt, cnt_pat);
+
+	if (flag)
+	{
+		return true;
+	}
+
+	for (int i = n; i < m; i++)
+	{
+		if (isSame(cnt_txt, cnt_pat))
+		{
+			return true;
+		}
+		cnt_txt[txt[i]]++;
+		cnt_txt[txt[i - n]]--;
+	}
+
+	return false;
+}
+
+int main()
+{
+	string txt = "parthgarg";
+	string pat = "thar";
+
+	cout << isAnagramPresent(txt, pat) << endl;
+	return 0;
+}
