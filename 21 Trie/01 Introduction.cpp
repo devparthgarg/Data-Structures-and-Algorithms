85d3f5910eedea3345349d2f11191255691428f9
Parth Garg <parthgarg2013@gmail.com>
2022-04-25 18:11:32 +0530

    trie

---
+++ 21 Trie/01 Introduction.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	/*
+	
+-->	Efficient for following 
+	operations on words of dictionary:
+
+	1. search
+	2. insert
+	3. delete
+	4. prefix search
+	5. lexiographical ordering of words
+
+-->	Comparsion of Trie and Hashing
+
+	
+	Function			|	Trie						|	Hashing
+-----------------------------------------------------------------------------	
+	search				|	O(word_len) worst case		|	O(word_len) average case
+
+	insert				|		"						|		"
+
+	delete				|		"						|		"
+
+	prefix search		|	O(word_len+output_len)		|	Not Supported
+
+	lexiographical		|	O(output_len)				|		"
+	ordering			|								|
+
+	*/
+	return 0;
+}
