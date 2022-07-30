bca5457cd0105f2797bfe91fc54b52fc79e88d4f
Parth Garg <parthgarg2013@gmail.com>
2022-07-30 16:50:38 +0530

    arrays

---
+++ 05. Arrays/Introduction.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+
+    Arrays Advantages:
+    1.random access
+    2.cache friendly
+    
+    Types of Arrays:
+    1.fixed size
+    eg: 
+        int arr[100] stack allocated
+        int arr[n] stack allocated
+        int arr[]={10,20,30} stack allocated
+        int *arr=new int[n] heap allocated
+    
+    2.dynamic size
+    eg:
+        vector<int> v heap allocated
+    
+    */
+
+    return 0;
+}
